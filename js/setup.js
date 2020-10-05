2ecf0cf5a75e30442d1c1cd934abe998b5aaafa1
dwd0497 <dwd0497@gmail.com>
2020-10-06 00:59:39 +0500

    Исправил замечания

--- js/setup.js
+++ js/setup.js
@@ -2,28 +2,36 @@
 
 document.querySelector(`.setup`).classList.remove(`hidden`);
 document.querySelector(`.setup-similar`).classList.remove(`hidden`);
+let wizardsListElement = document.querySelector(`.setup-similar-list`);
 
-const WIZARDS_NAMES = [`Иван`, `Хуан`, `Себастьян`, `Мария`, `Кристоф`, `Виктор`, `Юлия`, `Люпита`, `Вашингтон`];
-const WIZARDS_SURNAMES = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
-const COAT_COLORS = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`,
-  `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
-const EYES_COLORS = [`black`, `red`, `blue`, `yellow`, `green`];
 
-function renderWizards() {
-  let fragment = document.createDocumentFragment();
+const Wizards = {
+  NAMES: [`Иван`, `Хуан`, `Себастьян`, `Мария`, `Кристоф`, `Виктор`, `Юлия`, `Люпита`, `Вашингтон`],
+  SURNAMES: [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`],
+  COAT_COLORS: [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`,
+    `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`],
+  EYES_COLORS: [`black`, `red`, `blue`, `yellow`, `green`]
+};
+
+const wizardTemplateElement = document.querySelector(`#similar-wizard-template`).content.querySelector(`div`);
+
+function renderWizards(wizards) {
+  const fragment = document.createDocumentFragment();
 
   for (let i = 0; i < wizards.length; i++) {
-    fragment.appendChild(createWizard(i));
+    const wizard = wizards[i];
+    fragment.appendChild(createWizardElement(wizard));
   }
-  document.querySelector(`.setup-similar-list`).appendChild(fragment);
+
+  wizardsListElement.appendChild(fragment);
 }
 
-function createWizard(i) {
-  let wizard = document.querySelector(`#similar-wizard-template`).content.querySelector(`div`).cloneNode(true);
-  wizard.querySelector(`.setup-similar-label`).textContent = wizards[i].name;
-  wizard.querySelector(`.wizard-coat`).style.fill = wizards[i].coatColor;
-  wizard.querySelector(`.wizard-eyes`).style.fill = wizards[i].eyesColor;
-  return wizard;
+function createWizardElement(wizard) {
+  const wizardElement = wizardTemplateElement.cloneNode(true);
+  wizardElement.querySelector(`.setup-similar-label`).textContent = wizard.name;
+  wizardElement.querySelector(`.wizard-coat`).style.fill = wizard.coatColor;
+  wizardElement.querySelector(`.wizard-eyes`).style.fill = wizard.eyesColor;
+  return wizardElement;
 }
 
 function getRandomNumber(min, max) {
@@ -32,28 +40,41 @@ function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function getWizardName() {
-  return getRandomNumber(0, 10) > 5 ?
-    `${WIZARDS_NAMES[getRandomNumber(0, 5)]} ${WIZARDS_SURNAMES[getRandomNumber(0, 5)]}` :
-    `${WIZARDS_SURNAMES[getRandomNumber(0, 5)]} ${WIZARDS_NAMES[getRandomNumber(0, 5)]}`;
+let getRandomElement = function (elements) {
+  const randomIndex = getRandomNumber(0, elements.length - 1);
+  return elements[randomIndex];
+};
+
+function isMedian(elementsOne, elementsTwo) {
+  const oneLength = elementsOne.length;
+  const twoLength = elementsTwo.length;
+  const sum = oneLength + twoLength;
+  const half = Math.round(sum / 2);
+
+  return getRandomNumber(0, sum) > half;
 }
 
-function getWizard() {
+function getWizardName() {
+  const name = getRandomElement(Wizards.NAMES);
+  const surname = getRandomElement(Wizards.SURNAMES);
+
+  return isMedian(Wizards.NAMES, Wizards.SURNAMES) ? `${name} ${surname}` : `${surname} ${name}`;
+}
+
+function createRandomWizard() {
   return {
     name: getWizardName(),
-    coatColor: COAT_COLORS[getRandomNumber(0, 5)],
-    eyesColor: EYES_COLORS[getRandomNumber(0, 5)]
+    coatColor: Wizards.COAT_COLORS[getRandomElement(Wizards.COAT_COLORS)],
+    eyesColor: Wizards.EYES_COLORS[getRandomElement(Wizards.EYES_COLORS)]
   };
 }
 
-function getWizards() {
-  let wizardsArr = [];
+function createRandomWizards() {
+  const wizards = [];
   for (let i = 0; i < 4; i++) {
-    wizardsArr.push(getWizard());
+    wizards.push(createRandomWizard());
   }
-  return wizardsArr;
+  return wizards;
 }
 
-let wizards = getWizards();
-
-renderWizards();
+renderWizards(createRandomWizards());
